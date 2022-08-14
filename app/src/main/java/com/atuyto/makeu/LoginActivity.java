a7bf956ec708a361f92205c8e671f9bda1fd83a9
Alexandre <alexandresoune@gmail.com>
2022-08-14 15:57:31 +0200

    création de la popup d'inscription

--- app/src/main/java/com/atuyto/makeu/LoginActivity.java
+++ app/src/main/java/com/atuyto/makeu/LoginActivity.java
@@ -4,19 +4,25 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import com.atuyto.makeu.PopUp.SignInPopUp;
+
 public class LoginActivity extends AppCompatActivity {
 
     private RelativeLayout loginButton;
-    private RelativeLayout SignIngButton;
+    private RelativeLayout signingButton;
     private Intent mainActivity;
     private Activity activity;
+    private TextView SignInpopup_FirstName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,8 +34,16 @@ public class LoginActivity extends AppCompatActivity {
 
         mainActivity = new Intent(LoginActivity.this, MainActivity.class);
         loginButton = findViewById(R.id.BLogIn);
+        signingButton = findViewById(R.id.BSignUp);
 
 
+
+        signingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createSignInPopUp();
+            }
+        });
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,14 +55,16 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        SignIngButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
 
-            }
-        });
+    }
+
+    public void createSignInPopUp(){
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this, R.style.CustomDialogTheme);
+        final View SignInpopup = getLayoutInflater().inflate(R.layout.popup_signin, null);
+        SignInpopup_FirstName = (TextView) SignInpopup.findViewById((R.id.text_firstName));
 
 
+        dialogBuilder.setView(SignInpopup).create().show();
 
     }
 }
--- app/src/main/java/com/atuyto/makeu/PopUp/SignInPopUp.java
+++ app/src/main/java/com/atuyto/makeu/PopUp/SignInPopUp.java
@@ -1,16 +1,20 @@
 package com.atuyto.makeu.PopUp;
 
+import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
 
 import androidx.annotation.NonNull;
+
+import com.atuyto.makeu.R;
 
 public class SignInPopUp extends Dialog {
 
 
     private String title;
 
-    public SignInPopUp(@NonNull Context context) {
-        super(context);
+    public SignInPopUp(Activity activity){
+        super(activity, com.google.android.material.R.style.Base_ThemeOverlay_AppCompat);
+        setContentView(R.layout.popup_signin);
     }
 }
