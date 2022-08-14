d785f4b6b394966104cf34db23c4f2f0e6013e78
alexandre <alexandresoune@gmail.com>
2022-08-14 11:03:32 +0200

    new version

--- app/src/main/java/com/atuyto/makeu/PopUp/SignInPopUp.java
+++ app/src/main/java/com/atuyto/makeu/PopUp/SignInPopUp.java
@@ -1,10 +1,16 @@
 package com.atuyto.makeu.PopUp;
 
 import android.app.Dialog;
+import android.content.Context;
+
+import androidx.annotation.NonNull;
 
 public class SignInPopUp extends Dialog {
 
 
     private String title;
-    
+
+    public SignInPopUp(@NonNull Context context) {
+        super(context);
+    }
 }
